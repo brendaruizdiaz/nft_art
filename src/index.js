918b421eb3d1127af2a4fa1bd1410e975d67df54
Brenda <bvruizdiaz@hotmail.com>
2022-07-25 20:53:34 -0300

    update

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ const express = require('express');
 const morgan = require('morgan');
 const exphbs = require('express-handlebars');
 const path = require('path');
+const flash = require('connect-flash');
+const session = require('express-session');
+const MySqlStore = require('express-mysql-session');
+
+const { database } = require('./keys');
 
 // Initializations 
 const app = express();
@@ -20,12 +25,21 @@ app.set ('views', path.join(__dirname, 'views'))
 app.set('view engine', '.hbs');
 
 // Middlewares
+app.use(session({
+    secret: 'nftmysqlnodesession',
+    resave: false,
+    saveUninitialized: false,
+    store: new MySqlStore(database)
+}));
+app.use(flash());
 app.use(morgan('dev'));
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
+
 // Variables globales
 app.use((req, res, next) => {
+    app.locals.success = req.flash('success');
     next();
 });
 
--- src/routes/links.js
+++ src/routes/links.js
@@ -7,8 +7,49 @@ router.get('/add', (req, res) => {
     res.render('links/add');
 });
 
-router.post('/add', (req, res) => {
-    res.send('recibido');
+router.post('/add', async (req, res) => {
+    const { titulo, url, descripcion } = req.body;
+    const newLink = {
+        titulo,
+        url,
+        descripcion
+    };
+    await pool.query('INSERT INTO links set ?', [newLink]); 
+    req.flash('success', 'Link guardado correctamente');
+    res.redirect('/links');
+});
+
+router.get('/', async (req, res) => {
+    const links = await pool.query('SELECT * FROM links');
+    console.log(links);
+    res.render('links/list', { links });
+});
+
+router.get('/borrado/:id', async (req, res) => {
+    const { id } = req.params;
+    await pool.query('DELETE FROM links WHERE ID = ?', [id]);
+    req.flash('success', 'Link borrado correctamente');
+    res.redirect('back');
+});
+
+router.get('/editar/:id', async (req, res) => {
+    const { id } = req.params;
+    const links = await pool.query('SELECT * FROM links WHERE id = ?', [id]);
+    res.render('links/edit', {link: links[0]});
+});
+
+router.post('/editar/:id', async (req, res) => {
+    const { id } = req.params;
+    const { titulo, descripcion, url } = req.body;
+    const newLink = {
+        titulo,
+        descripcion,
+        url
+    };
+    console.log(newLink);
+    await pool.query('UPDATE links set ? WHERE id = ?', [newLink, id]);
+    req.flash('success', 'Link editado correctamente');
+    res.redirect('/links');
 });
 
 module.exports = router;
