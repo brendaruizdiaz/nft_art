41daf745bc98a0475437e99ebd246db5d87fffd5
Brenda <bvruizdiaz@hotmail.com>
2022-08-02 18:05:47 -0300

    new

--- src/routes/autenticacion.js
+++ src/routes/autenticacion.js
@@ -1,4 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/signup', (req, res) => {
+    res.render('auth/signup');
+});
+
+router.post('/signup', (req, res) => {
+    console.log(req.body);
+    res.send('Recibido');
+});
+
 module.exports = router;
